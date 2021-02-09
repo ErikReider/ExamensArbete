20a5b0393c60efc9ade2183299fd243c53b601bd
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-02-09 22:34:59 +0100

    Fixed WebGL not starting and now prints instead of writing results

--- Benchmark/Assets/EndScreen/scripts/EndScreen.cs
+++ Benchmark/Assets/EndScreen/scripts/EndScreen.cs
@@ -4,28 +4,39 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class EndScreen : MonoBehaviour
 {
     void writeResults()
     {
-        if (FPSCounter.instance.averageFPSData.Count == 0) return;
-        String resultPath = Directory.GetCurrentDirectory() + "/Results/";
-        String fileName = "result_" + SystemInfo.graphicsDeviceType + "_";
-        // To prevent overriding the old results folder
-        if (!Directory.Exists(resultPath)) Directory.CreateDirectory(resultPath);
         List<String> fpsData = FPSCounter.instance.averageFPSData.Select(s => s.ToString()).ToList();
-        string[] files = Directory.GetFiles(resultPath, fileName + "*");
-        // Gets the next index to use
-        int index = 0;
-        foreach (var file in files)
+        if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES3)
         {
-            String number = file.Replace(resultPath + fileName, "").Replace(".txt", "");
-            if (!number.All(char.IsDigit)) continue;
-            int fileIndex = int.Parse(number);
-            if (fileIndex > index) index = fileIndex;
+            if (FPSCounter.instance.averageFPSData.Count == 0) return;
+            String resultPath = Directory.GetCurrentDirectory() + "/Results/";
+            String fileName = "result_" + SystemInfo.graphicsDeviceType + "_";
+            // To prevent overriding the old results folder
+            if (!Directory.Exists(resultPath)) Directory.CreateDirectory(resultPath);
+            string[] files = Directory.GetFiles(resultPath, fileName + "*");
+            // Gets the next index to use
+            int index = 0;
+            foreach (var file in files)
+            {
+                String number = file.Replace(resultPath + fileName, "").Replace(".txt", "");
+                if (!number.All(char.IsDigit)) continue;
+                int fileIndex = int.Parse(number);
+                if (fileIndex > index) index = fileIndex;
+            }
+            File.WriteAllLines(resultPath + fileName + (index + 1).ToString() + ".txt", fpsData);
         }
-        File.WriteAllLines(resultPath + fileName + (index + 1).ToString() + ".txt", fpsData);
+        else
+        {
+            foreach (var data in fpsData)
+            {
+                print(data);
+            }
+        }
     }
     void Start()
     {
